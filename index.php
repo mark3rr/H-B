647d3b4eec5954a069ea22cba49097adae0081e3
Markus <markus.herr85@gmail.com>
2014-11-06 12:42:52 +0100

    Daten hinzugefügt

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title></title>
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet/less" type="text/css" href="css/style.less">
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/less.js"></script>
+<script type="text/javascript" src="js/functions.js"></script>
+</head>
+<body>
+</body>
+</html>
